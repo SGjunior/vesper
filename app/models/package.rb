39394a0148740bbc54fa47fb12ec7a6b2b9895e0
Adam Frej <awfrej@googlemail.com>
2018-05-29 10:26:32 -0400

    updated

--- app/models/package.rb
+++ app/models/package.rb
@@ -4,5 +4,5 @@ class Package < ApplicationRecord
 
   validates :name, :description, :price, :available_per_night, presence: true
   validates :price, :available_per_night, numericality: { only_integer: true, greater_than: 0 }
-  validates :venue_id, uniqueness: true
+  # validates :venue_id, uniqueness: true
 end
