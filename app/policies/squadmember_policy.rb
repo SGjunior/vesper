d09b3e010fe207e043031728cf83e8c3ca8bb0c4
Simon Guertin <sim.guertin@gmail.com>
2018-06-04 17:06:51 -0400

    massive commit a

--- app/policies/squadmember_policy.rb
+++ app/policies/squadmember_policy.rb
@@ -8,4 +8,25 @@ class SquadmemberPolicy < ApplicationPolicy
   def confirm_squad_member?
     record.user == user
   end
+
+  def member_choose_venue?
+    record.user == user
+  end
+
+  def member_confirm_venue?
+    record.user == user
+  end
+
+  def member_choose_contribution?
+    record.user == user
+  end
+
+  def member_confirm_contribution?
+    record.user == user
+  end
+
+  def choose_venue_status?
+    record.user == user
+  end
+
 end
