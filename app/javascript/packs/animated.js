da947e9d1a6d2c0bac13efd4d13287b019a36906
Simon Guertin <sim.guertin@gmail.com>
2018-06-05 11:49:45 -0400

    adding progress so far, issue with horizontal sidebar

--- app/javascript/packs/animated.js
+++ app/javascript/packs/animated.js
@@ -41,18 +41,115 @@ if (document.querySelector('.banner-main')) {
     // }, 2000);
   }
 
-  if (document.querySelector('.banner-main')) {
+  if (document.getElementById('mdl-slider')) {
 
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("contribution-input");
-    output.innerHTML = slider.value; // Display the default slider value
-
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() {
-      output.innerHTML = this.value;
-    }
+      // setInterval(function(){
 
 
+        // document.getElementById('container-lols').removeAttribute('hidden');
+        // var ProgressBar = require('progressbar');
 
+        var bar = new ProgressBar.Path('#heart-path', {
+          easing: 'bounce',
+          duration: 750
+        });
+
+        bar.set(0);
+        // bar.animate(1);  // Number from 0.0 to 1.0
+
+        var bar2 = new ProgressBar.Path('#heart-path2', {
+          easing: 'bounce',
+          duration: 750
+        });
+
+        bar2.set(0);
+        bar2.animate(0);  // Number from 0.0 to 1.0
+
+
+        var bar3 = new ProgressBar.Path('#hearth-path0', {
+          easing: 'bounce',
+          duration: 750
+        });
+
+        bar3.set(0);
+        // bar3.animate(1);  // Number from 0.0 to 1.0
+
+      // }, 2000);
+      if (document.getElementById('mdl-slider')) {
+
+      const packageCards = document.querySelectorAll('.package-card');
+      // console.log(packageCards);
+      const firstPackageCard = packageCards[0]
+      const secondPackageCard = packageCards[1]
+      const thirdPackageCard = packageCards[2]
+
+      const firstPath = document.getElementById('heart-path');
+      const secondPath = document.getElementById('heart-path2');
+      const thirdPath = document.getElementById('heart-path0');
+
+      var slider = document.getElementById("mdl-slider");
+      var output = document.getElementById("contribution-input");
+      output.innerHTML = slider.value; // Display the default slider value
+
+      // Update the current slider value (each time you drag the slider handle)
+      slider.oninput = function() {
+
+
+
+        output.innerHTML = this.value;
+        // console.log(this.value /100);
+        if (this.value < 33) {
+          if ((this.value /25) < 1) {
+            bar.animate(this.value /25);
+            bar2.animate(0);
+            bar3.animate(0);
+            firstPackageCard.classList.add('grayscale');
+            secondPackageCard.classList.add('grayscale');
+            thirdPackageCard.classList.add('grayscale');
+          }
+          firstPackageCard.style.zIndex = 2;
+
+
+        } else if ( this.value < 63) {
+          if (((this.value /25) - 1.2) < 1) {
+            bar.animate(1);
+            bar2.animate((this.value /25) - 1.2);
+            bar3.animate(0);
+            firstPackageCard.classList.remove('grayscale');
+            thirdPackageCard.classList.add('grayscale');
+          }
+          firstPackageCard.style.zIndex = 1;
+
+        } else {
+          if (((this.value /25) - 2.4) < 1) {
+            bar3.animate((this.value /25) - 2.4);
+          }
+
+          if (this.value > 90) {
+            thirdPackageCard.classList.remove('grayscale');
+            bar3.animate(1);
+          }
+          bar.animate(1);
+          bar2.animate(1);
+          firstPackageCard.classList.remove('grayscale');
+          secondPackageCard.classList.remove('grayscale');
+        }
+
+        // if ((this.value /100) < 0.30) {
+        //   bar2.animate(this.value /25);
+        //   console.log(this.value /25);
+        // } else if ((this.value /100) < 0.60){
+        //   if ((this.value /25 - 0.30) <= 1) {
+        //     bar.animate(this.value /25 - 0.30);
+        //     console.log(this.value /25 - 0.30);
+        //   }
+        // } else {
+        //   if ((this.value /25 - 0.60) <= 1) {
+        //     bar3.animate(this.value /25 - 0.60);
+        //     console.log(this.value /25 - 0.60);
+        //   }
+        // }
+      }
   }
+    }
 });
