b8b979f3fb5b8f0bafa0ba17f7f671e50eadcbd8
Simon Guertin <sim.guertin@gmail.com>
2018-06-06 13:41:37 -0400

    finalized the cards on the index page

--- app/assets/javascripts/channels/components/navbar.js
+++ app/assets/javascripts/channels/components/navbar.js
@@ -1,16 +1,16 @@
 
-window.onscroll = function() {stickyNavbar()};
+// window.onscroll = function() {stickyNavbar()};
 
-var navbar = document.querySelector('.navbar-wagon');
+// var navbar = document.querySelector('.navbar-wagon');
 
-var sticky = navbar.offsetTop;
+// var sticky = navbar.offsetTop;
 
-function stickyNavbar() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function stickyNavbar() {
+//   if (window.pageYOffset > sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
 
